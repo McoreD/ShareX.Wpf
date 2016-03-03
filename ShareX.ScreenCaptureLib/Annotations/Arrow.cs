1f378ba658c94afa47688b2c7b5f6451b5d5fcae
Michael Delpach <mcored@gmail.com>
2016-03-04 07:45:21 +0800

    Adding Line and Arrow

---
+++ ShareX.ScreenCaptureLib/Annotations/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ShareX.ScreenCaptureLib
+{
+    public class Arrow : Line
+    {
+        internal override void CacheDefiningGeometry()
+        {
+            Point startPoint = new Point(X1, Y1);
+            Point endPoint = new Point(X2, Y2);
+
+            var length = Math.Sqrt(Math.Pow(endPoint.Y - startPoint.Y, 2) + Math.Pow(endPoint.X - startPoint.X, 2));
+            var f = length / 15d;
+            var f2 = length / 30d;
+
+            var arrowBase = new Point(endPoint.X - (endPoint.X - startPoint.X) / f2, endPoint.Y - (endPoint.Y - startPoint.Y) / f2);
+            var arrowTip1 = new Point(arrowBase.X + (endPoint.Y - startPoint.Y) / f, arrowBase.Y - (endPoint.X - startPoint.X) / f);
+            var arrowTip2 = new Point(arrowBase.X - (endPoint.Y - startPoint.Y) / f, arrowBase.Y + (endPoint.X - startPoint.X) / f);
+            var arrowTipExt1 = new Point(arrowBase.X + (endPoint.Y - startPoint.Y) / f2, arrowBase.Y - (endPoint.X - startPoint.X) / f2);
+            var arrowTipExt2 = new Point(arrowBase.X - (endPoint.Y - startPoint.Y) / f2, arrowBase.Y + (endPoint.X - startPoint.X) / f2);
+
+            var arrow = new PathFigure(startPoint, new[] { new PolyLineSegment(new[] { arrowTip1, arrowTipExt1, endPoint, arrowTipExt2, arrowTip2 }, true), }, true)
+            {
+                IsFilled = true
+            };
+            cachedGeometry = new PathGeometry(new[] { arrow }, FillRule.Nonzero, null);
+        }
+    }
+}
---
+++ ShareX.ScreenCaptureLib/Annotations/Line.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace ShareX.ScreenCaptureLib
+{
+    public class Line : Annotate
+    {
+        protected Geometry cachedGeometry;
+
+        internal static bool IsDoubleFinite(object o)
+        {
+            double d = (double)o;
+            return (!double.IsInfinity(d) && !double.IsNaN(d));
+        }
+
+        public Line()
+        {
+            Stroke = Brushes.Blue;
+            StrokeThickness = 3;
+            StrokeLineJoin = PenLineJoin.Round;
+            StrokeStartLineCap = PenLineCap.Flat;
+            StrokeEndLineCap = PenLineCap.Flat;
+            Fill = Brushes.YellowGreen;
+
+            Effect = new DropShadowEffect
+            {
+                RenderingBias = RenderingBias.Quality,
+                Opacity = 0.8d,
+                Color = Color.FromRgb(10, 10, 10),
+                ShadowDepth = 20,
+                BlurRadius = 10
+            };
+        }
+
+        public static readonly DependencyProperty X1Property = DependencyProperty.Register("X1", typeof(double), typeof(Line),
+            new FrameworkPropertyMetadata(0d, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender),
+            new ValidateValueCallback(IsDoubleFinite));
+
+        public static readonly DependencyProperty Y1Property = DependencyProperty.Register("Y1", typeof(double), typeof(Line),
+                new FrameworkPropertyMetadata(0d, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender),
+                new ValidateValueCallback(IsDoubleFinite));
+
+        public static readonly DependencyProperty X2Property = DependencyProperty.Register("X2", typeof(double), typeof(Line),
+            new FrameworkPropertyMetadata(0d, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender),
+            new ValidateValueCallback(IsDoubleFinite));
+
+        public static readonly DependencyProperty Y2Property = DependencyProperty.Register("Y2", typeof(double), typeof(Line),
+            new FrameworkPropertyMetadata(0d, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender),
+            new ValidateValueCallback(IsDoubleFinite));
+
+        [TypeConverter(typeof(LengthConverter))]
+        public double X1
+        {
+            get { return (double)GetValue(X1Property); }
+            set { SetValue(X1Property, value); }
+        }
+
+        [TypeConverter(typeof(LengthConverter))]
+        public double Y1
+        {
+            get { return (double)GetValue(Y1Property); }
+            set { SetValue(Y1Property, value); }
+        }
+
+        [TypeConverter(typeof(LengthConverter))]
+        public double X2
+        {
+            get { return (double)GetValue(X2Property); }
+            set { SetValue(X2Property, value); }
+        }
+
+        [TypeConverter(typeof(LengthConverter))]
+        public double Y2
+        {
+            get { return (double)GetValue(Y2Property); }
+            set { SetValue(Y2Property, value); }
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                if (cachedGeometry == null)
+                {
+                    CacheDefiningGeometry();
+                }
+                return cachedGeometry;
+            }
+        }
+
+        internal virtual void CacheDefiningGeometry()
+        {
+            Point point1 = new Point(X1, Y1);
+            Point point2 = new Point(X2, Y2);
+
+            cachedGeometry = new LineGeometry(point1, point2);
+        }
+    }
+}
