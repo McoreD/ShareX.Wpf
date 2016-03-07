9e3e898899eba4e36c61ab072bb7b174eb3b49ad
Jaex <Jaex@users.noreply.github.com>
2016-03-07 13:04:50 +0200

    Added shadow to ellipse

--- ShareX.ScreenCaptureLib/Annotations/EllipseAnnotation.cs
+++ ShareX.ScreenCaptureLib/Annotations/EllipseAnnotation.cs
@@ -6,11 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace ShareX.ScreenCaptureLib
 {
     public class EllipseAnnotation : Annotation
     {
+        public int ShadowSize { get; set; } = 15;
+
         protected override Geometry DefiningGeometry
         {
             get
@@ -31,6 +34,23 @@ namespace ShareX.ScreenCaptureLib
             Fill = Brushes.Transparent;
             Stroke = brush;
             StrokeThickness = 1;
+
+            Effect = new DropShadowEffect
+            {
+                RenderingBias = RenderingBias.Quality,
+                Opacity = 0.8d,
+                Color = Colors.Black,
+                ShadowDepth = 0,
+                BlurRadius = ShadowSize
+            };
+        }
+
+        public override DrawingVisual Render()
+        {
+            DrawingVisual visual = new DrawingVisual();
+            visual.Effect = Effect;
+
+            return visual;
         }
     }
 }
